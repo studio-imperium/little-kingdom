892f1d3d070a56ce65cc78c478ffcf14e7373a57
William <williamqmckenzie@gmail.com>
2026-05-18 09:20:24 -0400

    fighter

--- build/build-all.sh
+++ build/build-all.sh
@@ -1,4 +1,18 @@
 version=2
 
+// build go binary
+GOOS=linux GOARCH=amd64 go build -o backend
+
+// add to dockrer
 docker build --platform linux/amd64 -t williamqm/mmobackend:latest .
-docker push williamqm/mmobackend:v2
+docker push williamqm/mmobackend:latest
+
+
+docker compose down
+rm -rf /root/Caddyfile
+cat > /root/Caddyfile <<'EOF'
+server.kingdomcrushers.com {
+reverse_proxy backend:8082
+}
+EOF
+docker compose up -d
